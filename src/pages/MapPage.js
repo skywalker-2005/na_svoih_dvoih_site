06990d1f02834c196f97ec3d634b17deedf2c8f6
skywalker-2005 <dimanj2005@gmail.com>
2024-09-02 16:26:10 +0300

    map

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,18 +1,18 @@
-import '../styles/style.css';
-import search from '../svg/search.svg';
-import sear_map from '../svg/sear_map.svg';
-import bear from '../svg/bear.svg';
-import tree from '../svg/tree.svg';
-import otkrArr from '../svg/otkr_arr.svg';
-import museum from '../svg/museum.svg';
-import plus from '../svg/plus.svg';
-import minus from '../svg/minus.svg';
-import navi from '../svg/navi.svg';
-import background from '../png/Screenshot 2024-05-07 at 16.32 1.png';
+import "../styles/style.css";
+import search from "../svg/search.svg";
+import sear_map from "../svg/sear_map.svg";
+import bear from "../svg/bear.svg";
+import tree from "../svg/tree.svg";
+import otkrArr from "../svg/otkr_arr.svg";
+import museum from "../svg/museum.svg";
+import plus from "../svg/plus.svg";
+import minus from "../svg/minus.svg";
+import navi from "../svg/navi.svg";
+import background from "../../qgis2web_2024_09_02-15_42_38_916932";
 
 const MapPage = () => {
-  const imgStyle = { width: '1vw', marginRight: '0.5vw' };
-  const imgStyleLarge = { width: '1.8vw' };
+  const imgStyle = { width: "1vw", marginRight: "0.5vw" };
+  const imgStyleLarge = { width: "1.8vw" };
 
   return (
     <>
@@ -20,7 +20,7 @@ const MapPage = () => {
         <img
           src={background}
           alt=""
-          style={{ width: '100%', height: '100%' }}
+          style={{ width: "100%", height: "100%" }}
         />
       </a>
       <div className="search_wrap">
@@ -34,29 +34,47 @@ const MapPage = () => {
           </div>
           <div className="search_item sit3">Все</div>
           <div className="search_item sit4">
-            <img src={bear} alt="" style={{ ...imgStyle, width: '1.3vw', marginRight: '0.5vw' }} />
+            <img
+              src={bear}
+              alt=""
+              style={{ ...imgStyle, width: "1.3vw", marginRight: "0.5vw" }}
+            />
             Заповедники
           </div>
           <div className="search_item sit5">
-            <img src={tree} alt="" style={imgStyle} />Национальные парки
+            <img src={tree} alt="" style={imgStyle} />
+            Национальные парки
           </div>
           <div className="search_item sit6">
-            <img src={otkrArr} alt="" style={imgStyle} />Тропы
+            <img src={otkrArr} alt="" style={imgStyle} />
+            Тропы
           </div>
           <div className="search_item sit7">
-            <img src={museum} alt="" style={{ ...imgStyle, width: '1.1vw' }} />Достопримечательности
+            <img src={museum} alt="" style={{ ...imgStyle, width: "1.1vw" }} />
+            Достопримечательности
           </div>
         </div>
       </div>
       <div className="plmi_wrap">
         <div className="plmi">
-          <div className="plmiitem"><img src={plus} alt="" className="plm" /></div>
-          <div className="plmiitem"><img src={minus} alt="" className="plm" style={{ paddingTop: '50%' }} /></div>
-          <div className="plmiitem"><img src={navi} alt="" className="plm" /></div>
+          <div className="plmiitem">
+            <img src={plus} alt="" className="plm" />
+          </div>
+          <div className="plmiitem">
+            <img
+              src={minus}
+              alt=""
+              className="plm"
+              style={{ paddingTop: "50%" }}
+            />
+          </div>
+          <div className="plmiitem">
+            <img src={navi} alt="" className="plm" />
+          </div>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default MapPage;

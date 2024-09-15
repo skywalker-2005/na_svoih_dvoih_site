ef9629ae3202d7fd51c9aa587c0cc5a441136e1d
Екатерина Савинкова <katy4.savinkova2004@yanedx.ru>
2024-09-15 14:28:36 +0300

    we have a live gallerygit statusgit statusgit status

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,16 +1,79 @@
+// import './style.css';
+
+// const Gallery = ({ images }) => {
+//   return (
+//     <div className="Gallery">
+//       {images.map((image, index) => (
+//         <img
+//           key={index}
+//           src={image}
+//           alt=''
+//           className="image"
+//         />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Gallery;
+import { useState } from 'react';
 import './style.css';
 
 const Gallery = ({ images }) => {
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
+  const openImage = (index) => {
+    setSelectedIndex(index);
+  };
+
+  const closeImage = () => {
+    setSelectedIndex(null);
+  };
+
+  const showNextImage = (e) => {
+    e.stopPropagation(); // Остановка всплытия события, чтобы не закрывать модальное окно
+    if (selectedIndex < images.length - 1) {
+      setSelectedIndex(selectedIndex + 1);
+    }
+  };
+
+  const showPrevImage = (e) => {
+    e.stopPropagation();
+    if (selectedIndex > 0) {
+      setSelectedIndex(selectedIndex - 1);
+    }
+  };
+
   return (
-    <div className="Gallery">
-      {images.map((image, index) => (
-        <img
-          key={index}
-          src={image}
-          alt=''
-          className="image"
-        />
-      ))}
+    <div>
+      <div className="Gallery">
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt=''
+            className="image"
+            onClick={() => openImage(index)}
+          />
+        ))}
+      </div>
+
+      {selectedIndex !== null && (
+        <div className="modal" onClick={closeImage}>
+          <button className="prev-button" onClick={showPrevImage}>
+            &#10094;
+          </button>
+          <img
+            src={images[selectedIndex]}
+            alt=''
+            className="modal-image"
+            onClick={(e) => e.stopPropagation()} // Остановка клика по изображению
+          />
+          <button className="next-button" onClick={showNextImage}>
+            &#10095;
+          </button>
+        </div>
+      )}
     </div>
   );
 };

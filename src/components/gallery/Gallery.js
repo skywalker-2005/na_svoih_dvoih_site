0bbf354ea67df2dfa190136e8790fb1c7c02fa31
Екатерина Савинкова <katy4.savinkova2004@yanedx.ru>
2024-09-15 14:40:10 +0300

    add lazy download

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -16,6 +16,69 @@
 // };
 
 // export default Gallery;
+// import { useState } from 'react';
+// import './style.css';
+
+// const Gallery = ({ images }) => {
+//   const [selectedIndex, setSelectedIndex] = useState(null);
+
+//   const openImage = (index) => {
+//     setSelectedIndex(index);
+//   };
+
+//   const closeImage = () => {
+//     setSelectedIndex(null);
+//   };
+
+//   const showNextImage = (e) => {
+//     e.stopPropagation(); // Остановка всплытия события, чтобы не закрывать модальное окно
+//     if (selectedIndex < images.length - 1) {
+//       setSelectedIndex(selectedIndex + 1);
+//     }
+//   };
+
+//   const showPrevImage = (e) => {
+//     e.stopPropagation();
+//     if (selectedIndex > 0) {
+//       setSelectedIndex(selectedIndex - 1);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <div className="Gallery">
+//         {images.map((image, index) => (
+//           <img
+//             key={index}
+//             src={image}
+//             alt=''
+//             className="image"
+//             onClick={() => openImage(index)}
+//           />
+//         ))}
+//       </div>
+
+//       {selectedIndex !== null && (
+//         <div className="modal" onClick={closeImage}>
+//           <button className="prev-button" onClick={showPrevImage}>
+//             &#10094;
+//           </button>
+//           <img
+//             src={images[selectedIndex]}
+//             alt=''
+//             className="modal-image"
+//             onClick={(e) => e.stopPropagation()} // Остановка клика по изображению
+//           />
+//           <button className="next-button" onClick={showNextImage}>
+//             &#10095;
+//           </button>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Gallery;
 import { useState } from 'react';
 import './style.css';
 
@@ -31,7 +94,7 @@ const Gallery = ({ images }) => {
   };
 
   const showNextImage = (e) => {
-    e.stopPropagation(); // Остановка всплытия события, чтобы не закрывать модальное окно
+    e.stopPropagation();
     if (selectedIndex < images.length - 1) {
       setSelectedIndex(selectedIndex + 1);
     }
@@ -50,9 +113,10 @@ const Gallery = ({ images }) => {
         {images.map((image, index) => (
           <img
             key={index}
-            src={image}
+            src={image} // Используем одно и то же изображение
             alt=''
             className="image"
+            loading="lazy" // Ленивую загрузку оставляем для оптимизации
             onClick={() => openImage(index)}
           />
         ))}
@@ -64,7 +128,7 @@ const Gallery = ({ images }) => {
             &#10094;
           </button>
           <img
-            src={images[selectedIndex]}
+            src={images[selectedIndex]} // Полные изображения для модального окна
             alt=''
             className="modal-image"
             onClick={(e) => e.stopPropagation()} // Остановка клика по изображению
